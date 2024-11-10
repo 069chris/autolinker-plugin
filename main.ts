af9a11e1b105b26e105d8316636e18f63eac66a8
069chris <chris@chrismuehli.de>
2024-11-10 11:31:00 +0100

    First Commit

--- main.ts
+++ main.ts
@@ -1,134 +1,75 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	FileSystemAdapter,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+	Vault,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class AutoLinkPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "auto-link-words",
+			name: "Auto-link Words from open file to Existing Files",
+			hotkeys: [],
+			callback: () => this.autoLinkWords(this.getActiveFile()),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+			id: "auto-link-files",
+			name: "Auto-link files to each other",
+			hotkeys: [],
+			callback: () => this.linkAllFiles(),
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	getActiveFile(): TFile | null {
+		return this.app.workspace.getActiveFile();
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	extractWords(text: string): string[] {
+		//new Notice(`${text.match(/(?<!\[\[)\b[äöüÄÖÜßa-zA-Z]{2,}\b(?!\]\])/g)}`)
+		return text.match(/(?<!\[\[)\b[äöüÄÖÜßa-zA-Z]{2,}\b(?!\]\])/g) || [];
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+
+	linkAllFiles(){
+		const allFiles = this.app.vault.getMarkdownFiles();
+		allFiles.forEach(file => {
+			this.autoLinkWords(file)
+		});
+	}
+
+	async autoLinkWords(file: TFile| null) {
+		if (!file) return;
+
+		// Get all files in the vault
+		const allFiles = this.app.vault.getMarkdownFiles();
+		const fileNames = new Set(allFiles.map((file) => file.basename));
+
+		// Read content of the active file
+		const fileContent = await this.app.vault.read(file);
+
+		// Regex to match words that are not already linked and consist only of letters (incl. Umlaute)
+		const wordRegex = /\b(?!\[\[)([äöüÄÖÜßa-zA-Z]{2,})(?!\]\])\b/g;
+
+		// Process each match and add links where applicable
+		const updatedContent = fileContent.replace(wordRegex, (match) => {
+			// Check if the match corresponds to an existing file name and is not already linked
+			if (fileNames.has(match)) {
+				return `[[${match}]]`; // Add double brackets around the word to link it
+			}
+			return match; // Return the word unchanged if no match
+		});
+
+		// Update the file with new content
+		await this.app.vault.modify(file, updatedContent);
+		new Notice("Auto-Linking Complete!");
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
